37839c115ca01d981f5d8b634f980b1d7aed77f1
Harsh Barsaiyan <hbarsaiyan@gmail.com>
2022-10-22 23:39:43 +0530

    add testimonials

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       xl: "1440px",
     },
     extend: {
+      fontFamily: {
+        'DMSans': ["DM Sans", "sans-serif"]
+      },
       colors: {
         brightRed: 'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
