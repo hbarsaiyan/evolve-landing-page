583d0fac9aca7cf2e407d6d150d6843331d75da1
Harsh Barsaiyan <hbarsaiyan@gmail.com>
2022-10-25 16:08:04 +0530

    remove redundant code

--- dist/script.js
+++ dist/script.js
@@ -5,12 +5,9 @@ let cross = document.getElementById("cross");
 
 btn.addEventListener("click", function () {
 	menu.classList.toggle("hidden");
-	menu.classList.toggle("visible");
 	menu.classList.toggle("flex");
 	ham.classList.toggle("hidden");
-	ham.classList.toggle("visible");
 	cross.classList.toggle("hidden");
-	cross.classList.toggle("visible");
 });
 
 if (
